0e283f8151aca2cd286ad730b65b704e8a0738c3
Samuel Sampaio <samukasmk@gmail.com>
2020-04-12 02:42:41 -0300

    Creating simple admin screens

--- mybills/admin.py
+++ mybills/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from mybills.models import Account, Expense, Income, Transfer
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'account_type')
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('description', 'value', 'date', 'account', 'is_payed')
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('description', 'value', 'date', 'account', 'is_payed')
+
+
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ('description', 'value', 'date', 'source_account', 'destination_account', 'is_payed')
