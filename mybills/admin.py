81678bba3155800b014b7a160728392489ab97f4
Samuel Sampaio <samukasmk@gmail.com>
2020-04-19 18:32:26 -0300

    Improving admin views with list filters by date

--- mybills/admin.py
+++ mybills/admin.py
@@ -5,18 +5,22 @@ from mybills.models import Account, Expense, Income, Transfer
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'account_type')
+    list_filter = ('account_type',)
 
 
 @admin.register(Expense)
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('date', 'description', 'value', 'account', 'is_payed')
+    list_filter = ('is_payed', 'date', 'account')
 
 
 @admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('date', 'description', 'value', 'account', 'is_payed')
+    list_filter = ('is_payed', 'date', 'account')
 
 
 @admin.register(Transfer)
 class TransferAdmin(admin.ModelAdmin):
     list_display = ('date', 'description', 'value', 'source_account', 'destination_account', 'is_payed')
+    list_filter = ('is_payed', 'date', 'source_account', 'destination_account')
